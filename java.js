f31f18508e3e93a1046c631746a9ba97d060849c
Matias Felipe Whiat <felipewhiat@gmail.com>
2024-11-19 09:40:09 -0300

    commit
    commit

--- java.js
+++ java.js
@@ -1,39 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const preloader = document.getElementById('preloader');
-    const content = document.getElementById('content');
+// Mostrar el preloader mientras carga la página
+document.body.classList.add('loading');
 
-    // Espera 3 segundos antes de ocultar el preloader
-    setTimeout(function() {
-        // Ocultar el preloader
-        preloader.style.display = 'none';
-
-        // Mostrar el contenido de la página
-        content.style.display = 'block';
-    }, 7000); // 4000 milisegundos = 4 segundos
+// Detectar cuando la página ha cargado
+window.addEventListener('load', function () {
+    document.body.classList.remove('loading');
+    document.body.classList.add('loaded');
 });
-
-
-document.getElementById('alertText').style.color = '#ff0000';
-
-
-let lastScrollTop = 0;
-const translatorContainer = document.getElementById('translator_container');
-
-window.addEventListener('scroll', function() {
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-    if (scrollTop > lastScrollTop) {
-        // Usuario bajando el cursor
-        translatorContainer.classList.add('hidden');
-    } else {
-        // Usuario subiendo el cursor
-        translatorContainer.classList.remove('hidden');
-    }
-
-    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Evita valores negativos
-});
-
-document.getElementById('closeAlert').addEventListener('click', function() {
-    document.getElementById('alert').style.display = 'none'; // Ocultar alerta al hacer clic en el botón
-});
-
